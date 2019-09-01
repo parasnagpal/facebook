429d22c0dc6378dc039ccbccc9be97182f724718
Paras Nagpal <paras.nag@gmail.com>
2019-09-01 16:28:12 +0530

    login page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import {Navbar,NavbarBrand} from 'reactstrap'
+import {Navbar,NavbarBrand,Form,Button} from 'reactstrap'
+
 
 class Login extends React.Component{
     render(){
         return(
-            <Navbar expand="lg">
+            <Navbar expand="lg" className="d-flex justify-content-between">
                <NavbarBrand>facebook</NavbarBrand>
+               <div className="login-form">
+               <Form inline className="m-1">
+                 <label for="username">username</label>
+                 <input id="username"/>
+                </Form>
+                <Form inline className="m-1">
+                 <label for="password">password</label>
+                 <input type="password" id="password"/>
+               </Form>
+               <button className="mt-4 mb-1">Login</button>
+              
+               </div>
             </Navbar>
         );
     }
