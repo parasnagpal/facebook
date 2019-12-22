6cc4617c929c1a4cad447c2de436611b7582ddd5
Paras Nagpal <paras.nag@gmail.com>
2019-12-22 23:30:24 +0530

    (ui): UI improvements

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -9,18 +9,20 @@ class Comp extends React.Component{
     
     render(){
         return(
-            <Nav className='flex-column sidebar'>
-                <NavItem className='active d-flex justify-content-between'>
-                    <a href='/home' className='side-text'>Home</a>
-                    <FaEllipsisH/>
-                </NavItem>
-                <NavItem>
-                    <a href='/profile'>Profile</a>
-                </NavItem>
-                <NavItem>
-                    <a>Friends</a>
-                </NavItem>
-            </Nav>
+            <div className='small_display_off'>
+                <Nav className='flex-column sidebar'>
+                    <NavItem className='active d-flex justify-content-between'>
+                        <a href='/home' className='side-text'>Home</a>
+                        <FaEllipsisH/>
+                    </NavItem>
+                    <NavItem>
+                        <a href='/profile'>Profile</a>
+                    </NavItem>
+                    <NavItem>
+                        <a>Friends</a>
+                    </NavItem>
+                </Nav>
+            </div>
         );
     }
 };
