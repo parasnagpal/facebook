66dd10d2d558eb01c7db34ab5c5f3f1520ce0294
Paras Nagpal <paras.nag@gmail.com>
2019-12-28 17:18:32 +0530

    (style):formatting change

--- src/components/news_feed/poslayout.js
+++ src/components/news_feed/poslayout.js
@@ -8,18 +8,10 @@ import {MdDelete} from 'react-icons/md'
 
 import '../../assets/style/css/style.css'
 
-
-
-
-
-
-const postlayout=(props)=>{
+//Image post layout 
+function ImagePost(props){
     let date=new Date()
-    console.log(props.image_posts)
-
-    if(props.image_post){
-        
-        return(
+    return(
             <div className='post p-2' key={props.key}>
                 <div className='post-head d-flex justify-content-between'>
                     <div>{date.toDateString()}</div>
@@ -38,27 +30,41 @@ const postlayout=(props)=>{
                 </div>
                 <div><Input type='text' className='rounded-pill s-7 gray-back' id='comment' placeholder='Comment..' /></div>
             </div>
-            )
+    );
+}
+//text post layout
+function TextPost(props){
+    let date=new Date()
+    return (
+            <div className='post p-2' key={props.key}>
+                <div className='post-head d-flex justify-content-between'>
+                  <div>{date.toDateString()}</div>
+                  <div>
+                      <MdDelete className='icon mx-2 s-1'/>
+                      <FaEdit className='icon s-1'/>
+                  </div>
+                </div>
+                <div className='d-flex flex-column p-4'>
+                     <>{props.post}</>
+                     <div className=''></div>
+                </div>
+                <div className='d-flex justify-content-around border-top p-2'>
+                    <div className='icon d-flex s-9'><FaThumbsUp className='align-self-center'/><div className='align-self-center'>  Like</div></div> 
+                    <div className='icon s-9'><FaShare /> Share</div> 
+                </div>
+                <div><Input type='text' className='rounded-pill s-7 gray-back' id='comment' placeholder='Comment..' /></div>
+            </div>
+    );
+}
 
-   } 
+
+
+const postlayout=(props)=>{
+
+    if(props.image_post)
+        return ImagePost(props);
    
-   return (<div className='post p-2' key={props.key}>
-             <div className='post-head d-flex justify-content-between'>
-               <div>{date.toDateString()}</div>
-               <div>
-                   <MdDelete className='icon mx-2 s-1'/>
-                   <FaEdit className='icon s-1'/>
-               </div>
-             </div>
-             <div className='d-flex flex-column p-4'>
-                  <>{props.post}</>
-                  <div className=''></div>
-             </div>
-             <div className='d-flex justify-content-around border-top p-2'>
-                 <div className='icon d-flex s-9'><FaThumbsUp className='align-self-center'/><div className='align-self-center'>  Like</div></div> 
-                 <div className='icon s-9'><FaShare /> Share</div> 
-             </div>
-             <div><Input type='text' className='rounded-pill s-7 gray-back' id='comment' placeholder='Comment..' /></div>
-          </div>)
-  }
+    else return TextPost(props);
+   
+}
   export default postlayout
