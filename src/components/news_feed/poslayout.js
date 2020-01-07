46ea86d633a24b3b9eadccfe61ad0fb7a13876b1
Paras Nagpal <paras.nag@gmail.com>
2020-01-08 00:03:48 +0530

    (style):formatting changes

--- src/components/news_feed/poslayout.js
+++ src/components/news_feed/poslayout.js
@@ -16,8 +16,10 @@ function ImagePost(props){
                 <div className='post-head d-flex justify-content-between'>
                     <div>{date.toDateString()}</div>
                     <div>
+
                         <MdDelete className='icon mx-2 s-1'/>
                         <FaEdit className='icon s-1'/>
+
                     </div>
                 </div>
                 <div className='d-flex flex-column p-4'>
@@ -25,8 +27,18 @@ function ImagePost(props){
                     <div className=''></div>
                 </div>
                 <div className='d-flex justify-content-around border-top p-3'>
-                    <div className='icon d-flex s-9'><FaRegThumbsUp className='align-self-center'/><div className='align-self-center'>  Like</div></div> 
-                    <div className='icon s-9'><MdChatBubbleOutline /> Share</div> 
+                    <div className='icon d-flex s-9'>
+
+                        <FaRegThumbsUp className='align-self-center'/>
+                        <div className='align-self-center'>  Like</div>
+                    
+                    </div> 
+                    <div className='icon s-9'>
+                        
+                        <MdChatBubbleOutline /> 
+                        <div>Share</div>
+                    
+                    </div> 
                 </div>
                 <div><Input type='text' className='rounded-pill s-7 gray-back' id='comment' placeholder='Comment..' /></div>
             </div>
