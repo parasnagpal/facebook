f5ece0de53517e7745c88a073ad52a8fbba07ca7
Paras Nagpal <paras.nag@gmail.com>
2020-01-13 01:00:53 +0530

    (style): UI changes

--- src/components/news_feed/components/rightbar.js
+++ src/components/news_feed/components/rightbar.js
@@ -14,7 +14,15 @@ class Bar extends React.Component{
             <div className='d-flex flex-column'>
                 <div className='rightbar card-header'>
                     People You May Know
+                    <span className='d-flex width-fit'>
+                        <div className='border-top suggestions'>
+                            <div>Paras Nagpal</div>
+                            <div>Recommended for you</div>
+                        </div>
+                        <button>Add Friend</button>
+                    </span>
                 </div>
+                
             </div>
         );
     }
