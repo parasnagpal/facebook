99e935597cfb58e77f4d0579b6614df73f3d6863
Paras Nagpal <paras.nag@gmail.com>
2020-01-16 01:17:22 +0530

    (style): Sidebar UI Change

--- src/components/news_feed/components/sidebar.js
+++ src/components/news_feed/components/sidebar.js
@@ -8,7 +8,7 @@ class Comp extends React.Component{
         return(
             <div className='small_display_off'>
                 <Nav className='flex-column sidebar'>
-                    <NavItem className='active d-flex justify-content-between align-items-center px-2'>
+                    <NavItem className='active d-flex justify-content-start align-items-center px-2'>
                         <FaNewspaper/>
                         <a href='/home' className='side-text'>News Feed</a>
                     </NavItem>
