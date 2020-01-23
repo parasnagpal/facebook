c0b00bb1b701092f9c142ffd986df79875e7ce21
Paras Nagpal <paras.nag@gmail.com>
2020-01-24 04:57:59 +0530

    (feat): Profile view and update

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom'
 
+import {GoGlobe} from 'react-icons/go';
+import {MdChatBubbleOutline} from 'react-icons/md'
+
 class Profile extends React.Component{
     constructor(){
         super()
@@ -9,11 +12,41 @@ class Profile extends React.Component{
         //console.log(this.props)
         if(this.props.loggedin)
             return(
-                    <div className='d-flex justify-content-center flex-column'>
-                        <div className='cover-photo-container'>
-                            <img className='cover-photo' src={require('../../assets/data/unset-cover-photo.jpg')}/>
+                    <div className='d-flex justify-content-center'>
+                        <div className='d-flex flex-column'>
+
+                            <div className='d-flex flex-column'>
+                                <div className='cover-photo-container'>
+                                    <img className='cover-photo' src={require('../../assets/data/unset-cover-photo.jpg')}/>
+                                    <img className='profile-photo-enlarged' src={require('../../assets/data/default-profile-photo.jpg')}/>
+                                </div>
+                                <div className='cover-photo-footer d-flex flex-column'>
+                                    <div className='align-self-end p-2'>
+                                        <span className='m-2'>Friends</span>
+                                        <span className='m-2'>Photos</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='d-flex'>
+                                <div className='flex-grow-1'>
+                                    <div className='post m-2 p-2'>
+                                        <div className='d-flex align-items-center'>
+                                            <GoGlobe/>
+                                            <span className='ml-1'>Intro</span>
+                                        </div>
+                                        <div className='d-flex flex-column align-items-center'>
+                                            <MdChatBubbleOutline/>
+                                            <input placeholder='     Add a short bio to tell people more about yourself.' className='s'></input>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='flex-grow-2'>
+                                    <div className='post m-2'>ds</div>
+                                </div>
+                            </div>
+
                         </div>
-                        <img className='profile-photo-enlarged' src={require('../../assets/data/default-profile-photo.jpg')}/>
                     </div>
                    )
         else 
