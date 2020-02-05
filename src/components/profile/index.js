d419302954c20941a562b53341b98f7e55845be2
Paras Nagpal <paras.nag@gmail.com>
2020-02-06 01:03:01 +0530

    (style): UI/UX update

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -4,6 +4,8 @@ import {Redirect} from 'react-router-dom'
 import {GoGlobe} from 'react-icons/go';
 import {MdChatBubbleOutline} from 'react-icons/md'
 
+import CreatePost from '../news_feed/components/posts/createpost'
+
 class Profile extends React.Component{
     constructor(){
         super()
@@ -21,7 +23,7 @@ class Profile extends React.Component{
                                     <img className='profile-photo-enlarged' src={require('../../assets/data/default-profile-photo.jpg')}/>
                                 </div>
                                 <div className='cover-photo-footer d-flex flex-column'>
-                                    <div className='align-self-end p-2'>
+                                    <div className='align-self-center p-2'>
                                         <span className='m-2'>Friends</span>
                                         <span className='m-2'>Photos</span>
                                     </div>
@@ -30,19 +32,21 @@ class Profile extends React.Component{
 
                             <div className='d-flex'>
                                 <div className='flex-grow-1'>
-                                    <div className='post m-2 p-2'>
+                                    <div className='post m-2 ml-0 p-2'>
                                         <div className='d-flex align-items-center'>
                                             <GoGlobe/>
                                             <span className='ml-1'>Intro</span>
                                         </div>
                                         <div className='d-flex flex-column align-items-center'>
                                             <MdChatBubbleOutline/>
-                                            <input placeholder='     Add a short bio to tell people more about yourself.' className='s'></input>
+                                            <input placeholder='Add a short bio to tell people more about yourself.' className='s'></input>
                                         </div>
                                     </div>
                                 </div>
-                                <div className='flex-grow-2'>
-                                    <div className='post m-2'>ds</div>
+                                <div className='flex-grow-3'>
+                                    <CreatePost/>
+                                    <b className="s">Posts</b>
+                                    <div className='post m-2 mr-0'>ds</div>
                                 </div>
                             </div>
 
